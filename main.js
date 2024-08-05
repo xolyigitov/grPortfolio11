15b957cb39f2148885f7b2e097de7d3e94b43e36
Shahriyor <abrorovshahriyor9@gmail.com>
2024-08-05 17:48:02 +0500

    Main students corect

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-let slides = document.querySelectorAll('.main-students__first');
+// let slides = document.querySelectorAll('.main-students__first');
 
-function slideAnimate(active){
-    slides[active].classList.add('active');
+// function slideAnimate(active){
+//     slides[active].classList.add('active');
 
-    slides.forEach((slide) => {
-        slide.addEventListener('click', () => {
-            clearActive();
-            slide.classList.add('active');
-        })
-    })
-}
+//     slides.forEach((slide) => {
+//         slide.addEventListener('click', () => {
+//             clearActive();
+//             slide.classList.add('active');
+//         })
+//     })
+// }
 
-function clearActive(){
-    slides.forEach((item)=>item.classList.remove('active'))
-}
+// function clearActive(){
+//     slides.forEach((item)=>item.classList.remove('active'))
+// }
 
 
-slideAnimate(0);
+// slideAnimate(0);
